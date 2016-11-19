036136f7b6fa158bdf890a0c5db8a6f3fb6d53cf
Xiao <xliang1993@gmail.com>
2016-11-19 13:16:56 -0500

    untrach run.sh

--- Easy-MNISTClassifier/run.sh
+++
@@ -1,6 +0,0 @@
-cp ../../test/main.cpp main.cpp
-cp ../../test/MNISTClassifier.cpp MNISTClassifier.cpp
-cp ../../test/MNISTClassifier.h MNISTClassifier.h
-make clean
-make 
-./output/MNISTClassifier
